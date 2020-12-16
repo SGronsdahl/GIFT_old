dd610af22aec7f7d40d9db6903a1742e97dd3b70
Stefan Gronsdahl <stefan.gronsdahl@gmail.com>
2020-12-16 11:40:09 -0800

    add package dependencies

--- R/AvgHydraulics.R
+++ R/AvgHydraulics.R
@@ -17,6 +17,7 @@
 #' @export
 #' @return .csv and .jpeg of channel cross section if specified
 #' @return data frame of reach-averaged hydraulics
+#' @import dplyr
 #' AvgHydraulics()
 
 library(dplyr)
